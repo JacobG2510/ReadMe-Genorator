0330174d8004710b80e2ae17c0ff54b203843349
blastking2510@gmail.com <blastking2510@gmail.com>
2023-06-20 21:01:48 -0700

    basic set up and started building

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-// TODO: Include packages needed for this application
 
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: '',
+    message: "",
+    name: ""
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    inquirer.createPromptModule();
+    prompt(questions).then()
+}
 
 // TODO: Create a function to initialize app
 function init() {}
