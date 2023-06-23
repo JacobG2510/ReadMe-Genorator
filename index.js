1b8b9539e836abcf57b5189e3f47ce84ff714c61
blastking2510@gmail.com <blastking2510@gmail.com>
2023-06-22 17:34:07 -0700

    everything is set up and ready

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer")
-const generateMarkdown = require("./generateMarkdown");
-const myPackage = require('my-package');
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
-{
-    type: 'input',
-    message: "put message:",
-    name: "name",   
-},
-{
-    type: 'input',
-    message: "put message:",
-    name: "name",   
-},
-{
-    type: 'input',
-    message: "put message:",
-    name: "name",   
-}
-
+  {
+    type: "input",
+    message: "Enter the title of your project:",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter a description of your project:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions:",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Enter usage information:",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter contribution guidelines:",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Enter test instructions:",
+    name: "tests",
+  },
+  {
+    type: "list",
+    message: "Choose a license for your application:",
+    name: "license",
+    choices: ["MIT", "Apache-2.0", "GPL-3.0", "None"],
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username:",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "Enter your email address:",
+    name: "email",
+  },
 ];
 
 // TODO: Create a function to write README file
